db6e42da1f8aa00b83cbfea6dc1abce5e085638c
yongpinglooi <148317159+yongpinglooi@users.noreply.github.com>
2025-01-17 13:13:09 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  cloud {
+    hostname = "nlb-tfepoc-01-8e78469c7df03e24.elb.ap-southeast-1.amazonaws.com"
+    organization = "Power-Terra"
+
+    workspaces {
+      name = "tfe-17Jan"
+    }
+  }
+}
+
+
 provider "aws" {
   region = "us-west-2"
 }
