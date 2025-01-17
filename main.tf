3c373245a00bb62fcfd61d7b2186a243fbe71969
yongpinglooi <148317159+yongpinglooi@users.noreply.github.com>
2025-01-17 13:36:02 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,23 @@ terraform {
   }
 }
 
-
 provider "aws" {
   region = "us-west-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformExampleInstance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
 }
